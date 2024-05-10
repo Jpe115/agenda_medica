f98235c162874d174095e2e6e0cf0e4b4ede3e73
Josue Palafox <jpalafoxe90@gmail.com>
2024-05-10 11:55:50 -0700

    Screen de pacientes visualmente terminada

--- lib/presentation/screens/pacientes_screen.dart
+++ lib/presentation/screens/pacientes_screen.dart
@@ -1,3 +1,4 @@
+import 'package:agenda/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class PacientesScreen extends StatelessWidget {
@@ -13,7 +14,194 @@ class PacientesScreen extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text(name),
       ),
-      body: const Placeholder(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const _AddContainer(),
+
+            const SizedBox(height: 13,),
+
+            SizedBox(
+              height: 531,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 14),
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (BuildContext context, int index) {
+                    if (index == 0) {
+                    return const _TableHeaders();
+                    }
+                    return const _TableRows();
+                  },
+                ),
+              ),
+            ),
+
+            Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 14),
+                  child: Text("Mostrando registros del 1 al 10, del total de 10"),
+                ),
+                const Spacer(),
+                TextButton(onPressed: () {}, child: const Text("Anterior")),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: TextButton(onPressed: () {}, child: const Text("Siguiente")),
+                ),
+              ],
+            ),
+
+            Container(color: Colors.amber, height: 70,)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _TableHeaders extends StatelessWidget {
+  const _TableHeaders();
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textStyle = Theme.of(context).textTheme;
+
+    return Table(
+      border: TableBorder.all(width: 0.95),
+      children: [
+        TableRow(
+          decoration: BoxDecoration(color: Colors.deepPurple.shade100),
+          children: [
+            TableCell(child: Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Text("Nombre", style: textStyle.titleLarge),
+            )),
+            TableCell(child: Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Text("Apellidos", style: textStyle.titleLarge,),
+            )),
+            TableCell(child: Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Text("Edad", style: textStyle.titleLarge,),
+            )),
+            TableCell(child: Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Text("Teléfono", style: textStyle.titleLarge,),
+            )),
+            TableCell(child: Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Text("Correo", style: textStyle.titleLarge,),
+            )),
+            TableCell(child: Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Text("Acciones", style: textStyle.titleLarge,),
+            )),
+          ]
+        )
+      ],
+    );
+  }
+}
+
+class _TableRows extends StatelessWidget {
+  const _TableRows();
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textStyle = Theme.of(context).textTheme;
+
+    return Table(
+      border: TableBorder.all(width: 0.85),
+      children: [
+        TableRow(
+          children: [
+            TableCell(verticalAlignment: TableCellVerticalAlignment.middle, 
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 7),
+                child: Text("Josué", style: textStyle.bodyLarge,),
+              ),
+            ),
+            TableCell(verticalAlignment: TableCellVerticalAlignment.middle, 
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 7),
+                child: Text("Palafox Encinas", style: textStyle.bodyLarge,),
+              )
+            ),
+            TableCell(verticalAlignment: TableCellVerticalAlignment.middle, 
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 7),
+                child: Text("23", style: textStyle.bodyLarge,),
+              )
+            ),
+            TableCell(verticalAlignment: TableCellVerticalAlignment.middle, 
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 7),
+                child: Text("6531088628", style: textStyle.bodyLarge,),
+              )
+            ),
+            TableCell(verticalAlignment: TableCellVerticalAlignment.middle, 
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 7),
+                child: Text("jpalafoxe90@gmail.com", style: textStyle.bodyLarge,),
+              )
+            ),
+            TableCell(
+              child: Row(
+                children: [
+
+                  Padding(
+                    padding: const EdgeInsets.all(7.0),
+                    child: IconButton.filled(onPressed: () {}, icon: const Icon(Icons.edit_rounded)),
+                  ),
+
+                  IconButton.filled(onPressed: () {
+                    DeleteDialogs.deletePacienteDialog(context);
+                  }, icon: const Icon(Icons.delete_forever_rounded)),
+
+                ],
+              )
+            ),
+          ]
+        )
+      ],
+    );
+  }
+}
+
+class _AddContainer extends StatelessWidget {
+  const _AddContainer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(13.0),
+      child: Container(
+        height: 90,
+        width: double.infinity,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(7), color: Colors.grey.shade300),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 7, horizontal: 15),
+              child: Text("Listado de pacientes", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 13),
+              child: FilledButton.icon(
+                onPressed: () {
+                  AddDialogs.newPacienteDialog(context);
+                }, 
+                icon: const Icon(Icons.add_circle_outline_rounded),
+                label: const Text("Añadir nuevo paciente")
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
