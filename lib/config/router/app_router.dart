9b7ef36c7ed9f7dc3c160788216d87f2313d7a30
Josue Palafox <jpalafoxe90@gmail.com>
2024-05-09 21:49:35 -0700

    Rutas para screens creadas

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -7,8 +7,33 @@ final appRouter = GoRouter(
 
     GoRoute(
       path: "/",
-      name: ""  
-    )
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(),  
+    ),
+
+    GoRoute(
+      path: "/doctores",
+      name: DoctoresScreen.name,
+      builder: (context, state) => const DoctoresScreen(),  
+    ),
+
+    GoRoute(
+      path: "/pacientes",
+      name: PacientesScreen.name,
+      builder: (context, state) => const PacientesScreen(),  
+    ),
+
+    GoRoute(
+      path: "/especialidades",
+      name: EspecialidadesScreen.name,
+      builder: (context, state) => const EspecialidadesScreen(),  
+    ),
+
+    GoRoute(
+      path: "/citas",
+      name: CitasScreen.name,
+      builder: (context, state) => const CitasScreen(),  
+    ),
 
   ]
 );
