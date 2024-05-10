1559e88bc30f884193ea89a17651e79890aa844b
Josue Palafox <jpalafoxe90@gmail.com>
2024-05-10 00:58:49 -0700

    Cambio visual a delete doctor

--- lib/presentation/widgets/delete_dialogs.dart
+++ lib/presentation/widgets/delete_dialogs.dart
@@ -6,23 +6,8 @@ class DeleteDialogs {
     return showDialog(context: context, 
       builder: (context) {
         return(AlertDialog(
-          title: const Text("Form"),
-          content: SingleChildScrollView(
-            child: Column(
-              children: [
-                const Text("Lorem ut incididunt ea occaecat. Pariatur ullamco consequat ea in consectetur voluptate ut do et ut laborum proident. Laboris reprehenderit mollit duis consequat proident. Sit minim cupidatat magna dolor ad qui cillum sint duis anim nisi consequat magna dolor. Nostrud velit mollit cupidatat irure commodo irure aute laborum ipsum sit nostrud sit irure sunt. Eu ad reprehenderit ea dolore ullamco proident non anim. Sit reprehenderit sint est ipsum laborum do velit anim amet excepteur laboris adipisicing mollit."),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Nombre'),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Apellido'),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Especialidad'),
-                ),
-              ],
-            ),
-          ),
+          title: const Text("Eliminar doctor"),
+          content: const Text("¿Está seguro que desea eliminar al doctor seleccionado?"),
           actions: <Widget>[
             TextButton(
               child: const Text('Cancelar'),
@@ -33,7 +18,7 @@ class DeleteDialogs {
             
             FilledButton(onPressed: () {
               Navigator.of(context).pop();
-              }, child: const Text("Guardar")
+              }, child: const Text("Eliminar")
             )
           ],
         ));
