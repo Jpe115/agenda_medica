a8e75c5314728d517ff6202f82b17d750cf19f75
Josue Palafox <jpalafoxe90@gmail.com>
2024-05-09 21:35:00 -0700

    Menu items creado para rutas

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+
+class MenuItem{
+  final String title;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({required this.title, required this.link, required this.icon});
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+    title: "Citas", 
+    link: "/citas", 
+    icon: Icons.add
+  ),
+
+  MenuItem(
+    title: "Pacientes", 
+    link: "/pacientes", 
+    icon: Icons.smart_button_outlined
+  ),
+  
+  MenuItem(
+    title: "Doctores",  
+    link: "/doctores", 
+    icon: Icons.credit_card
+  ),
+
+  MenuItem(
+    title: "Especialidades", 
+    link: "/especialidades", 
+    icon: Icons.refresh_outlined
+  ),
+
+];
