76c6bf82653d9fe3b63b914c6daf5d7494d6a3f3
Josue Palafox <jpalafoxe90@gmail.com>
2024-05-09 21:38:58 -0700

    Exportaciones de screens en archivo de barril

---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,5 @@
+export 'package:agenda/presentation/screens/citas_screen.dart';
+export 'package:agenda/presentation/screens/doctores_screen.dart';
+export 'package:agenda/presentation/screens/especialidades_screen.dart';
+export 'package:agenda/presentation/screens/pacientes_screen.dart';
+export 'package:agenda/presentation/screens/home_screen.dart';
