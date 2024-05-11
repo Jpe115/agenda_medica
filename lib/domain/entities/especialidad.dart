693e581e7aedb7c8075a00f9d66be931ce6e60c3
Josue Palafox <jpalafoxe90@gmail.com>
2024-05-11 14:47:03 -0700

    Mapper añadido a clase especialidad

--- lib/domain/entities/especialidad.dart
+++ lib/domain/entities/especialidad.dart
@@ -7,4 +7,11 @@ class Especialidad {
     required this.id, 
     required this.nombreEspecialidad
   });
+
+  factory Especialidad.fromJson(Map<String, dynamic> json) {
+    return Especialidad(
+      nombreEspecialidad: json['especialidad'],
+      id: json['id'],
+    );
+  }
 }
