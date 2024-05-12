2fbc7ccc89438978243e29a8ed54beea5dbae764
Josue Palafox <jpalafoxe90@gmail.com>
2024-05-11 21:09:50 -0700

    Función delete añadida a especialidades datasource y repository de domain

--- lib/domain/datasources/especialidades_datasource.dart
+++ lib/domain/datasources/especialidades_datasource.dart
@@ -1,7 +1,9 @@
 import 'package:agenda/domain/entities/especialidad.dart';
+import 'package:agenda/domain/entities/py_response.dart';
 
 abstract class EspecialidadesDatasource {
 
   Future<List<Especialidad>> getEspecialidades();
 
+  Future<PyResponse> deleteEspecialidad(int id);
 }
