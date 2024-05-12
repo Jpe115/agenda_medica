e5373fda855ac090244f7f7ae965a00d1759e5a2
Josue Palafox <jpalafoxe90@gmail.com>
2024-05-11 17:05:15 -0700

    Datsource para doctores

---
+++ lib/infrastructure/datasources/doctor_pydb_datasource.dart
@@ -0,0 +1,28 @@
+import 'package:agenda/domain/datasources/doctores_datasource.dart';
+import 'package:agenda/domain/entities/doctor.dart';
+import 'package:dio/dio.dart';
+
+class DcotorPydbDatasource extends DoctoresDatasource {
+
+  final dio = Dio(BaseOptions(
+      baseUrl: "http://127.0.0.1:5000/api"
+  ));
+
+  @override
+  Future<List<Doctor>> getDoctores() async{
+
+    final response = await dio.get("/doctores");
+
+    // Asegúrate de que response.data es una List
+    if (response.data is List) {
+      List<Doctor> doctores = (response.data as List).map((item) {
+        return Doctor.fromJson(item);
+      }).toList();
+
+      return doctores;
+    } else {
+      throw Exception('Expected a list');
+    }
+  }
+
+}
