9b12f6fb15f061288bc53b366cf65b8af809615a
Josue Palafox <jpalafoxe90@gmail.com>
2024-05-11 17:05:03 -0700

    Añadido mapper para y desde json en clase doctor

--- lib/domain/entities/doctor.dart
+++ lib/domain/entities/doctor.dart
@@ -3,11 +3,29 @@ class Doctor {
   final String nombre;
   final String apellidos;
   final int especialidadId;
+  final String especialidad;
 
   Doctor({
     required this.id, 
     required this.nombre, 
     required this.apellidos, 
-    required this.especialidadId
+    required this.especialidadId,
+    required this.especialidad
   });
+
+  factory Doctor.fromJson(Map<String, dynamic> json) => Doctor(
+        apellidos: json["apellido"],
+        especialidad: json["especialidad"],
+        especialidadId: json["especialidadId"],
+        id: json["id"],
+        nombre: json["nombre"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "apellido": apellidos,
+        "especialidad": especialidad,
+        "especialidadId": especialidadId,
+        "id": id,
+        "nombre": nombre,
+    };
 }
