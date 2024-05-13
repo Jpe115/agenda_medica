62cb8d3196d0c5e54f54d13118479472f05090ce
Josue Palafox <jpalafoxe90@gmail.com>
2024-05-13 00:24:01 -0700

    Añadido a input field tipo de teclado, máximo de caracteres y formato de texto

--- lib/presentation/widgets/add_dialogs.dart
+++ lib/presentation/widgets/add_dialogs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AddDialogs {
 
@@ -82,21 +83,38 @@ class AddDialogs {
           content: SingleChildScrollView(
             child: Column(
               children: [
-                const Text("Lorem ut incididunt ea occaecat. Pariatur ullamco consequat ea in consectetur voluptate ut do et ut laborum proident. Laboris reprehenderit mollit duis consequat proident. Sit minim cupidatat magna dolor ad qui cillum sint duis anim nisi consequat magna dolor. Nostrud velit mollit cupidatat irure commodo irure aute laborum ipsum sit nostrud sit irure sunt. Eu ad reprehenderit ea dolore ullamco proident non anim. Sit reprehenderit sint est ipsum laborum do velit anim amet excepteur laboris adipisicing mollit."),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 70),
+                  child: Text("Ingrese los datos del paciente."),
+                ),
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'Nombre'),
+                  keyboardType: TextInputType.name,
+                  maxLength: 30,
+                  inputFormatters: [FilteringTextInputFormatter.allow(RegExp("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]"))  ],
                 ),
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'Apellidos'),
+                  keyboardType: TextInputType.name,
+                  maxLength: 40,
+                  inputFormatters: [FilteringTextInputFormatter.allow(RegExp("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]"))  ],
                 ),
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'Edad'),
+                  keyboardType: TextInputType.number,
+                  maxLength: 3,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                 ),
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'Teléfono'),
+                  keyboardType: TextInputType.phone,
+                  maxLength: 13,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                 ),
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'Correo'),
+                  keyboardType: TextInputType.emailAddress,
+                  maxLength: 60,
                 ),
               ],
             ),
