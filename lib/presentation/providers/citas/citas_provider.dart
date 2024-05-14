c23daf84eee02322f75179a782ee83bc5340fb9c
Josue Palafox <jpalafoxe90@gmail.com>
2024-05-14 15:22:48 -0700

    Corregido nombre de provider

--- lib/presentation/providers/citas/citas_provider.dart
+++ lib/presentation/providers/citas/citas_provider.dart
@@ -2,7 +2,7 @@ import 'package:agenda/domain/entities/cita.dart';
 import 'package:agenda/presentation/providers/citas/citas_repository_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final doctoresProvider = StateNotifierProvider<CitasNotifier, List<Cita>>((ref) {
+final citasProvider = StateNotifierProvider<CitasNotifier, List<Cita>>((ref) {
   final fetchCitas = ref.read(citasRepositoryProvider).getCitas;
 
   return CitasNotifier(fetchCitas: fetchCitas);
