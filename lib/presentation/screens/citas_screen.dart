cd41349a63f5b79cd3214c8d890b2432c72c8482
Josue Palafox <jpalafoxe90@gmail.com>
2024-05-15 18:18:12 -0700

    Formatear hora ahoar admite datos de 3 caracteres en vez de solo 4

--- lib/presentation/screens/citas_screen.dart
+++ lib/presentation/screens/citas_screen.dart
@@ -181,7 +181,10 @@ class _TableRows extends StatefulWidget {
 class _TableRowsState extends State<_TableRows> {
 
   String formatearHora(String hora) {
-    return '${hora.substring(0, 2)}:${hora.substring(2)}';
+    if (hora.length == 4) {
+      return '${hora.substring(0, 2)}:${hora.substring(2)}';
+    }
+    return '0${hora.substring(0, 1)}:${hora.substring(1)}';
   }
 
   @override
