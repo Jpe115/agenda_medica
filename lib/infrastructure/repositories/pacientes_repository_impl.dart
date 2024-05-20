90459256773cc5ca2bcfdc0a4c45296b51a2b97d
Josue Palafox <jpalafoxe90@gmail.com>
2024-05-20 02:05:26 -0700

    Cambiado pydb a oracle pydb datasource en repos

--- lib/infrastructure/repositories/pacientes_repository_impl.dart
+++ lib/infrastructure/repositories/pacientes_repository_impl.dart
@@ -1,11 +1,12 @@
 import 'package:agenda/domain/entities/paciente.dart';
 import 'package:agenda/domain/entities/py_response.dart';
 import 'package:agenda/domain/repositories/pacientes_repository.dart';
+import 'package:agenda/infrastructure/datasources/pacientes_oraclepydb_datasource.dart';
 import 'package:agenda/infrastructure/datasources/pacientes_pydb_datasource.dart';
 
 class PacientesRepositoryImpl extends PacientesRepository {
 
-  final PacientePydbDatasource datasource;
+  final PacienteOraclepydbDatasource datasource;
   PacientesRepositoryImpl({required this.datasource});
 
   @override
--- lib/presentation/providers/pacientes/pacientes_repository_provider.dart
+++ lib/presentation/providers/pacientes/pacientes_repository_provider.dart
@@ -1,7 +1,8 @@
+import 'package:agenda/infrastructure/datasources/pacientes_oraclepydb_datasource.dart';
 import 'package:agenda/infrastructure/datasources/pacientes_pydb_datasource.dart';
 import 'package:agenda/infrastructure/repositories/pacientes_repository_impl.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final pacientesRepositoryProvider = Provider((ref) {
-  return PacientesRepositoryImpl(datasource: PacientePydbDatasource());
+  return PacientesRepositoryImpl(datasource: PacienteOraclepydbDatasource());
 });
